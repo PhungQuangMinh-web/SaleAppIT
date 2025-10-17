5ca1d29d3c545faf86da6dad73538b4f3a2d63e9
PhungQuangMinh-web <2351010128minh@ou.edu.vn>
2025-10-17 15:32:47 +0700

    Add files via upload

---
+++ saleapp/index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import dao
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    q = request.args.get("q")
+    print(q)
+    cates = dao.load_categories()
+    prods = dao.load_products(q=q)
+    return render_template("index.html", cates = cates, prods = prods)
+
+if __name__ == "__main__":
+    with app.app_context():
+        app.run(debug=True)
